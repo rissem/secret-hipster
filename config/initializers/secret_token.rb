179810a9ac1402760d837df53630b600e4765ae7
Mike Risse <rissem@gmail.com>
2012-06-02 10:18:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecretHipster::Application.config.secret_token = '28af7157097851ea43498ea3583963682d37b2f5857cadb02499bdac7e51b0c9c9455a2f2ef7f43dc89c7f12ffaef2b655cb11601a9cfdfcc0d25927c93b6d2f'
